64e87255b7a06f9de6207c918474c0886c390cf3
cuijianpeng <cuijianpeng@jodogame.com>
2016-08-03 16:38:38 +0800

    v0.1.0

---
+++ Example/Pods/Target Support Files/JDLib/JDLib-umbrella.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+#import "Request.pbobjc.h"
+#import "Response.pbobjc.h"
+
+FOUNDATION_EXPORT double JDLibVersionNumber;
+FOUNDATION_EXPORT const unsigned char JDLibVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-JDLib_Tests/Pods-JDLib_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_JDLib_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_JDLib_TestsVersionString[];
+
